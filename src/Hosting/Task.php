ef3ee7c26956ac4442fe5713392bd99dbc4a2f16
Sam Lerner <sam.lerner@acquia.com>
2016-01-07 11:59:00 -0500

    TAC-2878 - Initial commit

---
+++ src/Hosting/Task.php
@@ -0,0 +1,209 @@
+<?php
+
+/*
+ * This file is part of the Acquia Platform: Cloud Data Model.
+ *
+ * (c) Acquia, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Acquia\Platform\Cloud\Hosting;
+
+final class Server implements ServerInterface
+{
+    /**
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @var string
+     */
+    private $fullyQualifiedDomainName;
+
+    /**
+     * @var string
+     */
+    private $amiType;
+
+    /**
+     * @var string
+     */
+    private $ec2Region;
+
+    /**
+     * @var string
+     */
+    private $ec2AvailabilityZone;
+
+    /**
+     * @var array
+     */
+    private $services;
+
+    public function __construct($name)
+    {
+        if (!is_string($name) || !preg_match('#^[a-z0-9-]+$#i', $name)) {
+            throw new \InvalidArgumentException(
+                sprintf(
+                    '%s: Server name must be an alphanumeric string (%s given)',
+                    __METHOD__,
+                    is_string($name) ? $name : gettype($name)
+                )
+            );
+        }
+        $this->name = $name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function create(array $serverData)
+    {
+        $app = new static($serverData['name']);
+        if (isset($serverData['ami_type'])) {
+            $app->setAmiType($serverData['ami_type']);
+        }
+        if (isset($serverData['ec2_region'])) {
+            $app->setAmiType($serverData['ec2_region']);
+        }
+        if (isset($serverData['ec2_availability_zone'])) {
+            $app->setAmiType($serverData['ec2_availability_zone']);
+        }
+        if (isset($serverData['services']) && is_array($serverData['services'])) {
+            $app->setServices($serverData['services']);
+        }
+
+        return $app;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getFullyQualifiedDomainName()
+    {
+        return $this->name . ".prod.hosting.acquia.com";
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getAmiType()
+    {
+        if ($this->amiType === null) {
+            throw new \RuntimeException(
+                sprintf('%s: This Server object does not know the AMI Type.', __METHOD__)
+            );
+        }
+        return $this->amiType;
+    }
+
+    /**
+     * Add a string.
+     *
+     * @param string $amiType A string.
+     */
+    public function setAmiType($amiType)
+    {
+        if (!is_string($amiType) || empty($amiType)) {
+            throw new \InvalidArgumentException(
+                sprintf('%s: $amiType expects a string.', __METHOD__)
+            );
+        }
+        $this->amiType = $amiType;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEc2Region()
+    {
+        if ($this->ec2Region === null) {
+            throw new \RuntimeException(
+              sprintf('%s: This Server object does not know the EC2 Region.', __METHOD__)
+            );
+        }
+        return $this->ec2Region;
+    }
+
+    /**
+     * Add a string.
+     *
+     * @param string $ec2Region A string.
+     */
+    public function setEc2Region($ec2Region)
+    {
+        if (!is_string($ec2Region) || empty($ec2Region)) {
+            throw new \InvalidArgumentException(
+              sprintf('%s: $ec2Region expects a string.', __METHOD__)
+            );
+        }
+        $this->ec2Region = $ec2Region;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getEc2AvailabilityZone()
+    {
+        if ($this->ec2AvailabilityZone === null) {
+            throw new \RuntimeException(
+              sprintf('%s: This Server object does not know the EC2 Availability Zone.', __METHOD__)
+            );
+        }
+        return $this->ec2AvailabilityZone;
+    }
+
+    /**
+     * Add a string.
+     *
+     * @param string $ec2AvailabilityZone A string.
+     */
+    public function setEc2AvailabilityZone($ec2AvailabilityZone)
+    {
+        if (!is_string($ec2AvailabilityZone) || empty($ec2AvailabilityZone)) {
+            throw new \InvalidArgumentException(
+              sprintf('%s: $ec2AvailabilityZone expects a string.', __METHOD__)
+            );
+        }
+        $this->ec2AvailabilityZone = $ec2AvailabilityZone;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function getServices()
+    {
+        if ($this->services === null) {
+            throw new \RuntimeException(
+              sprintf('%s: This Server object does not know the services array.', __METHOD__)
+            );
+        }
+        return $this->services;
+    }
+
+    /**
+     * Add an array.
+     *
+     * @param array $services An array.
+     */
+    public function setServices($services)
+    {
+        if (!is_array($services) || empty($services)) {
+            throw new \InvalidArgumentException(
+              sprintf('%s: $ec2AvailabilityZone expects an array.', __METHOD__)
+            );
+        }
+        $this->services = $services;
+    }
+}
---
+++ src/Hosting/TaskInterface.php
@@ -0,0 +1,191 @@
+<?php
+
+/*
+ * This file is part of the Acquia Platform: Cloud Data Model.
+ *
+ * (c) Acquia, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Acquia\Platform\Cloud\Hosting;
+
+/**
+ * A Task corresponds to a process running on our hosting. It contains:
+ * - An id, eg. 1234
+ * - Queue, for type of task, eg. code-push or files-migrate
+ * - State, where the task is at, eg. done or error
+ * - Description, what the task is, eg. "Deploy code to dev"
+ * - Created, timestamp when task was created
+ * - Started, timestamp when task actually started
+ * - Completed, timestamp when a task is completed, even unsuccessfully
+ * - Sender, who created the task, eg. cloud_api or user@acquia.com
+ * - Result, output returned when the task finishes
+ * - Cookie, cookie information @todo need to know more about what this is
+ * - Logs, a string with log information
+ */
+interface TaskInterface
+{
+    /**
+     * Factory method for Task classes.
+     *
+     * @param array $taskData
+     *
+     * @return TaskInterface
+     */
+    public static function create(array $taskData);
+
+    /**
+     * Returns the ID of the task.
+     *
+     * @return string The ID of the task.
+     */
+    public function getID();
+
+    /**
+     * Returns the queue name for the task. For example:
+     * - code-push
+     * - files-migrate
+     *
+     * @return string The queue name for the task.
+     */
+    public function getQueue();
+
+    /**
+     * Sets the queue name for the task.
+     *
+     * @param string $queue A string name for the queue.
+     */
+    public function setQueue($queue);
+
+    /**
+     * Returns the state of the task. For example:
+     *  - done
+     *  - error
+     *
+     * @return string State of the task.
+     */
+    public function getState();
+
+    /**
+     * Sets the state of the task.
+     *
+     * @param string $state State of the task.
+     */
+    public function setState($state);
+
+    /**
+     * Returns the description of the task. For example:
+     *  - Pushing code to dev
+     *
+     * @return string Description of the task.
+     */
+    public function getDescription();
+
+    /**
+     * Sets the description of the task.
+     *
+     * @param string $description Description of the task.
+     */
+    public function setDescription($description);
+
+    /**
+     * Returns the UNIX timestamp of when the task was created.
+     *
+     * @return int UNIX Timestamp when the task was created.
+     */
+    public function getCreated();
+
+    /**
+     * Sets the UNIX timestamp for when the task was created.
+     *
+     * @param int $created UNIX timestamp when the task was created.
+     */
+    public function setCreated($created);
+
+    /**
+     * Returns the UNIX timestamp of when the task was started.
+     *
+     * @return int UNIX Timestamp when the task was started.
+     */
+    public function getStarted();
+
+    /**
+     * Sets the UNIX timestamp for when the task was started.
+     *
+     * @param int $started UNIX timestamp when the task was started.
+     */
+    public function setStarted($started);
+
+    /**
+     * Returns the UNIX timestamp of when the task was completed.
+     *
+     * @return int UNIX Timestamp when the task was completed.
+     */
+    public function getCompleted();
+
+    /**
+     * Sets the UNIX timestamp for when the task was completed.
+     *
+     * @param int $completed UNIX timestamp when the task was completed.
+     */
+    public function setCompleted($completed);
+
+    /**
+     * Returns the sender, or creator, of the task. For example:
+     * - cloud_api
+     * - user@acquia.com
+     *
+     * @return string The sender of the task.
+     */
+    public function getSender();
+
+    /**
+     * Sets the sender for a task.
+     * @param string $sender The sender of the task.
+     */
+    public function setSender($sender);
+
+    /**
+     * Returns the result message after the task finishes.
+     *
+     * @return string Result of the task finishing.
+     */
+    public function getResult();
+
+    /**
+     * Sets the result when the task finishes.
+     *
+     * @param string $result The result message after the task completes.
+     */
+    public function setResult($result);
+
+    /**
+     * Returns the cookie for a task, if it exists.
+     *
+     * @return string Cookie information.
+     */
+    public function getCookie();
+
+    /**
+     * Sets the cookie information for a task.
+     *
+     * @param string $cookie Cookie information for the task.
+     */
+    public function setCookie($cookie);
+
+    /**
+     * Returns log information for the task.
+     *
+     * @return string Log information for the task.
+     */
+    public function getLogs();
+
+    /**
+     * Sets the log information for a task.
+     *
+     * @param string $logs Log information for the task.
+     */
+    public function setLogs($logs);
+}
