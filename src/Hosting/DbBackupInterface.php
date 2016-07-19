f069683d435a7129b0cce7f91ca3ac877d0b7087
Kirby Powell <kirbpowell@users.noreply.github.com>
2016-07-19 05:08:29 -0500

    TAC-4143 | acquia-platform-cloud-data-model needs a DbBackup class.

---
+++ src/Hosting/DbBackupInterface.php
@@ -0,0 +1,156 @@
+<?php
+
+/*
+ * This file is part of the Acquia Platform: Cloud Data Model.
+ *
+ * (c) Acquia, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Acquia\Platform\Cloud\Hosting;
+
+/**
+ * A DbBackup corresponds to a backup of a database for a client's application.
+ * It contains:
+ * - A backup ID eg. 12345678
+ * - A backup type, eg. daily, or ondemand
+ * - A name, eg. mysite
+ * - A link, eg. https://www.acquia.com/
+ * - A path, eg. backups/user-prod-db-backup.sql.gz
+ * - A started timestamp, eg. 1468218381
+ * - A completed timestamp, eg. 1468219381
+ * - A deleted marker, eg. 0 or 1
+ * - A checksum, eg. a9e3dac5c312f49415b613aff078f5dd
+ */
+interface DbBackupInterface
+{
+    /**
+     * Factory method for DbBackup classes.
+     *
+     * @param array $dbBackupData
+     *
+     * @return DbBackupInterface
+     */
+    public static function create(array $dbBackupData);
+
+    /**
+     * Returns the ID of the dbbackup.
+     *
+     * @return int The ID of the dbbackup.
+     */
+    public function getId();
+
+    /**
+     * Returns the type of the database backup.
+     *
+     * @return string The type of the database backup.
+     */
+    public function getType();
+    
+    /**
+     * Sets the type of the dbbackup.
+     *
+     * @param string $type The type of the dbbackup.
+     */
+    public function setType($type);
+
+    /**
+     * Returns the name for the database backup.
+     *
+     * @return int The name for the database backup.
+     */
+    public function getName();
+    
+    /**
+     * Sets the name of the dbbackup.
+     *
+     * @param int $siteId The name of the dbbackup.
+     */
+    public function setName($siteId);
+
+    /**
+     * Returns the Link for the database backup.
+     *
+     * @return int The Link for the database backup.
+     */
+    public function getLink();
+    
+    /**
+     * Sets the Link of the dbbackup.
+     *
+     * @param int $dbId The Link of the dbbackup.
+     */
+    public function setLink($dbId);
+
+    /**
+     * Returns the path for the database backup.
+     *
+     * @return string The path for the database backup.
+     */
+    public function getPath();
+    
+    /**
+     * Sets the path of the dbbackup.
+     *
+     * @param string $path The path of the dbbackup.
+     */
+    public function setPath($path);
+
+    /**
+     * Returns the started timestamp for the database backup.
+     *
+     * @return int The started timestamp for the database backup.
+     */
+    public function getStarted();
+    
+    /**
+     * Sets the started timestamp of the dbbackup.
+     *
+     * @param int $started The started timestamp of the dbbackup.
+     */
+    public function setStarted($started);
+
+    /**
+     * Returns the completed timestamp for the database backup.
+     *
+     * @return int The completed timestamp for the database backup.
+     */
+    public function getCompleted();
+    
+    /**
+     * Sets the completed timestamp of the dbbackup.
+     *
+     * @param int $completed The completed timestamp of the dbbackup.
+     */
+    public function setCompleted($completed);
+
+    /**
+     * Returns whether or not the database backup has been deleted.
+     *
+     * @return int whether or not the database backup has been deleted.
+     */
+    public function getDeleted();
+    
+    /**
+     * Sets the deleted value of the dbbackup.
+     *
+     * @param int $deleted The deleted value of the dbbackup.
+     */
+    public function setDeleted($deleted);
+
+    /**
+     * Returns the checksum for the database backup.
+     *
+     * @return string The checksum for the database backup.
+     */
+    public function getChecksum();
+    
+    /**
+     * Sets the checksum of the dbbackup.
+     *
+     * @param string $checksum The checksum of the dbbackup.
+     */
+    public function setChecksum($checksum);
+}
---
+++ tests/Hosting/DbBackup/DbBackupDecoratorMethodsTest.php
@@ -0,0 +1,119 @@
+<?php
+
+/*
+ * This file is part of the Acquia Platform: Cloud Data Model.
+ *
+ * (c) Acquia, Inc.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Acquia\Platform\Cloud\Tests\Hosting\DbBackup;
+
+use Acquia\Platform\Cloud\Hosting\DbBackup\DbBackupDecoratorMethods;
+use Acquia\Platform\Cloud\Hosting\DbBackupInterface;
+
+/**
+ * @coversDefaultClass \Acquia\Platform\Cloud\Hosting\DbBackup\DbBackupDecoratorMethods
+ */
+class DbBackupDecoratorMethodsTest extends \PHPUnit_Framework_TestCase
+{
+    const TEST_TRAIT = 'Acquia\Platform\Cloud\Hosting\DbBackup\DbBackupDecoratorMethods';
+    const TEST_CLASS = 'Acquia\Platform\Cloud\Hosting\DbBackupInterface';
+
+    /**
+     * @covers ::getDbBackup
+     * @covers ::setDbBackup
+     */
+    public function testDbBackupDecoratorMethodsMaySetDecoratedDbBackup()
+    {
+        /** @var DbBackupDecoratorMethods $mockTrait */
+        $mockTrait = $this->getMockForTrait(self::TEST_TRAIT);
+        /** @var DbBackupInterface $mockClass   */
+        $mockClass = $this->getMockBuilder(self::TEST_CLASS)->getMock();
+
+        $mockTrait->setDbBackup($mockClass);
+        $this->assertEquals($mockClass, $mockTrait->getDbBackup());
+    }
+
+    /**
+     * @covers ::getId
+     * @covers ::getType
+     * @covers ::getName
+     * @covers ::getLink
+     * @covers ::getPath
+     * @covers ::getStarted
+     * @covers ::getCompleted
+     * @covers ::getDeleted
+     * @covers ::getChecksum
+     * @dataProvider getDbBackupInterfaceGetters
+     */
+    public function testDbBackupDecoratorMethodsPassesGettersToDbBackup($getter, $expected)
+    {
+        /** @var DbBackupDecoratorMethods $mockTrait */
+        $mockTrait = $this->getMockForTrait(self::TEST_TRAIT);
+        $mockClass = $this->getMockBuilder(self::TEST_CLASS)
+            ->getMock();
+        $mockClass->expects($this->once())
+            ->method($getter)
+            ->willReturn($expected);
+
+        $mockTrait->setDbBackup($mockClass);
+        $this->assertEquals($expected, call_user_func([$mockTrait, $getter]));
+    }
+
+    /**
+     * @covers ::setType
+     * @covers ::setName
+     * @covers ::setLink
+     * @covers ::setPath
+     * @covers ::setStarted
+     * @covers ::setCompleted
+     * @covers ::setDeleted
+     * @covers ::setChecksum
+     * @dataProvider getDbBackupInterfaceSetters
+     */
+    public function testDbBackupDecoratorMethodsPassesSettersToDbBackup($setter, $expected)
+    {
+        /** @var DbBackupDecoratorMethods $mockTrait */
+        $mockTrait = $this->getMockForTrait(self::TEST_TRAIT);
+        $mockClass = $this->getMockBuilder(self::TEST_CLASS)
+            ->getMock();
+        $mockClass->expects($this->once())
+            ->method($setter)
+            ->with($expected);
+
+        $mockTrait->setDbBackup($mockClass);
+        call_user_func([$mockTrait, $setter], $expected);
+    }
+
+    public function getDbBackupInterfaceGetters()
+    {
+        return [
+            ['getId', 12345678],
+            ['getType', 'daily'],
+            ['getName', 'mysite'],
+            ['getLink', 'http://www.acquia.com/'],
+            ['getPath', 'backups/user-prod-db-backup.sql.gz'],
+            ['getStarted', 1468218381],
+            ['getCompleted', 1468219381],
+            ['getDeleted', 0],
+            ['getChecksum', 'a9e3dac5c312f49415b613aff078f5dd'],
+        ];
+    }
+
+    public function getDbBackupInterfaceSetters()
+    {
+        return [
+            ['setType', 'daily'],
+            ['setName', 'mysite'],
+            ['setLink', 'http://www.acquia.com/'],
+            ['setPath', 'backups/user-prod-db-backup.sql.gz'],
+            ['setStarted', 1468218381],
+            ['setCompleted', 1468219381],
+            ['setDeleted', 0],
+            ['setChecksum', 'a9e3dac5c312f49415b613aff078f5dd'],
+        ];
+    }
+}
